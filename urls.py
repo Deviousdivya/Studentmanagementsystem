d1c69937881c2c52d8926d0b61b512d4cf46ce6d
Deviousdivya <1407divyabharat@gmail.com>
2026-02-02 18:08:56 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -3,10 +3,12 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-   path('',views.index, name="index"),
-   path('dashboard/', views.dashboard, name='dashboard'), 
-   path('notification/mark-as-read/', views.mark_notification_as_read, name='mark_notification_as_read' ),
-   path('notification/clear-all', views.clear_all_notification, name= "clear_all_notification")
+    path("", views.student_list, name='student_list'),
+    path("add/", views.add_student, name="add_student"),
+    path('students/<str:slug>/', views.view_student, name='view_student'),
+    path('edit/<str:slug>/', views.edit_student, name='edit_student'),
+    path('delete/<str:slug>/', views.delete_student, name='delete_student'),
 
+  
 
 ]
